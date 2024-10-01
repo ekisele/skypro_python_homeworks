74dcfcdc7f7d7a6d3201fa417bf2820018200031
ekisele <ekisele369@gmail.com>
2024-10-01 14:08:22 +0300

    lesson9

--- lesson_9/empltable.py
+++ lesson_9/empltable.py
@@ -2,36 +2,21 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.sql import text
 
 class EmplTable:
-    __scripts = {
-        "create": text("""
-            CREATE TABLE IF NOT EXISTS employee (
-                id SERIAL PRIMARY KEY,
-                is_active BOOLEAN DEFAULT TRUE,
-                create_timestamp TIMESTAMP DEFAULT now(),
-                change_timestamp TIMESTAMP DEFAULT now(),
-                first_name VARCHAR(20) NOT NULL,
-                last_name VARCHAR(20) NOT NULL,
-                middle_name VARCHAR(20),
-                phone VARCHAR(15) NOT NULL,
-                email VARCHAR(256),
-                avatar_url VARCHAR(1024),
-                company_id INTEGER NOT NULL
-            )
-        """),
-        "select": text("SELECT * FROM employee"),
-        "insert": text("""
-            INSERT INTO employee (first_name, last_name, phone, company_id)
-            VALUES (:first_name, :last_name, :phone, :company_id)
-        """),
-        "update": text("""
-            UPDATE employee SET first_name = :first_name, last_name = :last_name,
-            middle_name = :middle_name, phone = :phone, email = :email, avatar_url = :avatar_url
-            WHERE id = :employee_id
-        """),
-        "delete": text("DELETE FROM employee WHERE id = :employee_id"),
-        "delete_all": text("DELETE FROM employee"),
-        "get_employee_by_id": text("SELECT * FROM employee WHERE id = :employee_id")
-    }
+#     __scripts = {
+#         "select": text("SELECT * FROM employee"),
+#         "insert": text("""
+#             INSERT INTO employee (first_name, last_name, phone, company_id)
+#             VALUES (:first_name, :last_name, :phone, :company_id)
+#         """),
+#         "update": text("""
+#             UPDATE employee SET first_name = :first_name, last_name = :last_name,
+#             middle_name = :middle_name, phone = :phone, email = :email, avatar_url = :avatar_url
+#             WHERE id = :employee_id
+#         """),
+#         "delete": text("DELETE FROM employee WHERE id = :employee_id"),
+#         "delete_all": text("DELETE FROM employee"),
+#         "get_employee_by_id": text("SELECT * FROM employee WHERE id = :employee_id")
+#     }
 
     def __init__(self, connection_string):
         self.__db = create_engine(connection_string, encoding='utf-8', future=True)
@@ -58,6 +43,13 @@ class EmplTable:
             conn.commit()
             return result
 
+   
+    def get_employee_by_id(self, employee_id):
+        with self.__db.connect() as conn:
+            result = conn.execute(self.__scripts["get_employee_by_id"], {"employee_id": employee_id})
+            return result.fetchone()
+   
+   
     def update_employee(self, employee_id, first_name, last_name, middle_name, phone, email, avatar_url):
         with self.__db.connect() as conn:
             result = conn.execute(self.__scripts["update"],
@@ -70,11 +62,8 @@ class EmplTable:
                                    "avatar_url": avatar_url})
             conn.commit()
             return result
-
-    def get_employee_by_id(self, employee_id):
-        with self.__db.connect() as conn:
-            result = conn.execute(self.__scripts["get_employee_by_id"], {"employee_id": employee_id})
-            return result.fetchone()
+        
+    
 
     def delete_employee(self, employee_id):
         with self.__db.connect() as conn:
@@ -82,11 +71,7 @@ class EmplTable:
             conn.commit()
             return result
 
-    def delete_all_employees(self):
-        with self.__db.connect() as conn:
-            result = conn.execute(self.__scripts["delete_all"])
-            conn.commit()
-            return result
+
 
     def dispose(self):
         self.__db.dispose()
--- lesson_9/test_empl_db.py
+++ lesson_9/test_empl_db.py
@@ -3,7 +3,9 @@ from empltable import EmplTable
 import pytest
 
 api = Company("https://x-clients-be.onrender.com")
-db = EmplTable("postgresql+psycopg2://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyAYE31uq@dpg-cour99g21fec73bsgvug-a.oregon-postgres.render.com/x_clients_db_3fmx")
+db = EmplTable("postgresql+psycopg2://x_clients_user:ypYaT7FBULZv2VxrJuOHVoe78MEElWlb@dpg-crgp14o8fa8c73aritj0-a.frankfurt-postgres.render.com/x_clients_db_75hr")
+
+
 
 def setup_module(module):
     db.create_table()
